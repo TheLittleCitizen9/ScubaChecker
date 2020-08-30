0b52193f497c4aa3f534f51b13b1cacc9d2bb9b5
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-08-30 17:59:40 +0300

    Implemented the basic classes

--- ScubaChecker/ScubaChecker/TestsRunner.cs
+++ ScubaChecker/ScubaChecker/TestsRunner.cs
@@ -12,7 +12,6 @@ namespace ScubaChecker
         public T[] Deserialize(Stream stream)
         {
             XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
-            stream.Seek(0, SeekOrigin.Begin);
             object obj = xmlSerializer.Deserialize(stream);
             return (T[])obj;
         }
