cb413305f3001a141987284ef4fb99b30aeba8c6
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-08-30 17:58:25 +0300

    Implemented the basic classes

---
+++ ScubaChecker/ScubaChecker/Serializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+using MamasChecker.Abstract;
+
+namespace ScubaChecker
+{
+    public class Serializer<T> : ISerializer<T>
+    {
+        public Serializer()
+        {
+            
+        }
+
+
+        Stream ISerializer<T>.Serialize(T[] testsToSerialize)
+        {
+            MemoryStream stream = new MemoryStream();
+            XmlSerializer x = new XmlSerializer(typeof(T));
+            x.Serialize(stream, testsToSerialize);
+            return stream;
+        }
+    }
+}
---
+++ ScubaChecker/ScubaChecker/TestsRunner.cs
@@ -0,0 +1,20 @@
+﻿using MamasChecker.Abstract;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ScubaChecker
+{
+    public class TestsRunner<T> : ITestsRunner<T>
+    {
+        public T[] Deserialize(Stream stream)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+            stream.Seek(0, SeekOrigin.Begin);
+            object obj = xmlSerializer.Deserialize(stream);
+            return (T[])obj;
+        }
+    }
+}
